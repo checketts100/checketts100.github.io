4bb1bc1c4aa85a9aa07d316ae3dbb8d8274191ff
Checketts <J8547@csx.com>
2018-02-14 04:07:28 -0500

    working updates

---
+++ assignments/Lesson 6/WeatherDesign/scripts/currentdate.js
@@ -0,0 +1,14 @@
+/* Javascript code to product current date on Franklin Forecast webpage*/
+
+var theDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+var theMonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "Novemeber", "December"];
+
+var d = new Date();
+var dayName = theDays[d.getDay()];
+var day = d.getDate();
+var monthName = theMonth[d.getMonth()];
+var year = d.getFullYear()];
+
+document.getElementById("demoDate").innerHTML = "dayName" + ", " + day + " " + monthName + " " + year;
+
