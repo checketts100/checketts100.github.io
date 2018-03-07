5edadb54e9fc0c2ae11081345fcf53a12fb37a91
Checketts <J8547@csx.com>
2018-03-07 05:43:46 -0500

    historical data

--- assignments/Lesson9/scripts/cityInfo.js
+++ assignments/Lesson9/scripts/cityInfo.js
@@ -14,5 +14,21 @@ townInfo.onload = function() {
 	document.getElementById("townName").innerHTML = cityInfo.towns["0"].name;
 	document.getElementById("founded").innerHTML = cityInfo.towns["0"].yearFounded;
 	document.getElementById("moto").innerHTML = cityInfo.towns["0"].motto;
+	document.getElementById("population").innerHTML = cityInfo.towns["0"].currentPopulation;
+	document.getElementById("rain").innerHTML = cityInfo.towns["0"].averageRainfall;
+	document.getElementById("events").innerHTML = cityInfo.towns["0"].events;
 
+
+
+	document.getElementById("townName1").innerHTML = cityInfo.towns[1].name;
+	document.getElementById("founded1").innerHTML = cityInfo.towns[1].yearFounded;
+	document.getElementById("moto1").innerHTML = cityInfo.towns[1].motto;
+	document.getElementById("population1").innerHTML = cityInfo.towns[1].currentPopulation;
+	document.getElementById("rain1").innerHTML = cityInfo.towns[1].averageRainfall;
+	document.getElementById("events1").innerHTML = cityInfo.towns[1].events;
+	
+	
+	
+	
+	
 }
