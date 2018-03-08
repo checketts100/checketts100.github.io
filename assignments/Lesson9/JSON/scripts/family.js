610e51296bae889bb9fc6fe6608f57ffa1fc87c9
Checketts100 <Checketts100@users.noreply.github.com>
2018-03-08 13:56:16 -0500

    latest

--- assignments/Lesson9/JSON/scripts/family.js
+++ assignments/Lesson9/JSON/scripts/family.js
@@ -5,7 +5,14 @@ family .open("GET","https://checketts100.github.io/assignments/Lesson9/JSON/chec
 family .send();
 family .onload = function() {
 	
-	var familyData = JSON.parse(family .responseText);
+	var familyData = JSON.parse(family.responseText);
 	console.log(familyData);
+	
+	document.getElementById("wholename").innerHTML = familyData.members["0"].name;
+	document.getElementById("title").innerHTML = familyData.members["0"].familyTitle;
+	document.getElementById("age").innerHTML = familyData.members["0"].age;
+	document.getElementById("duties").innerHTML = familyData.members["0"].duties;
+	
+	
 	
 }
