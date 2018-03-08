2aa1dc694d0ae0c4a63ce40798829ac583243456
Checketts100 <Checketts100@users.noreply.github.com>
2018-03-08 12:45:48 -0500

    so far so good

---
+++ assignments/Lesson9/JSON/family.js
@@ -0,0 +1,14 @@
+//Javascript Document
+//CHECKETTS FAMILY
+var family = new XMLHttpRequest();
+
+family.open('GET','https://checketts100.github.io/assignments/Lesson9/JSON/checkettsfamily.json', true);
+
+family.send();
+
+family.onload = function() {
+	
+	var familyData = JSON.parse(family.responseText);
+	console.log(familyData);
+	
+	document.getElementById("familyName").innerHTML = cityInfo.towns["0"].name;
--- assignments/Lesson9/scripts/cityInfo.js
+++ assignments/Lesson9/scripts/cityInfo.js
@@ -1,11 +1,8 @@
 //Javascript Document
 //FRANKLIN, MN
 var townInfo = new XMLHttpRequest();
-
-townInfo.open('GET','https://byui-cit230.github.io/weather/data/towndata.json', true);
-
+townInfo.open("GET","https://byui-cit230.github.io/weather/data/towndata.json", true);
 townInfo.send();
-
 townInfo.onload = function() {
 	
 	var cityInfo = JSON.parse(townInfo.responseText);
@@ -18,17 +15,6 @@ townInfo.onload = function() {
 	document.getElementById("rain").innerHTML = cityInfo.towns["0"].averageRainfall;
 	document.getElementById("events").innerHTML = cityInfo.towns["0"].events;
 
+		
 
-
-	document.getElementById("townName1").innerHTML = cityInfo.towns[1].name;
-	document.getElementById("founded1").innerHTML = cityInfo.towns[1].yearFounded;
-	document.getElementById("moto1").innerHTML = cityInfo.towns[1].motto;
-	document.getElementById("population1").innerHTML = cityInfo.towns[1].currentPopulation;
-	document.getElementById("rain1").innerHTML = cityInfo.towns[1].averageRainfall;
-	document.getElementById("events1").innerHTML = cityInfo.towns[1].events;
-	
-	
-	
-	
-	
 }
