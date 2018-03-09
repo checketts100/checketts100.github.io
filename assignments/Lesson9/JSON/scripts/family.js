37271d38646b43d7fbbb9e8d881262cdda69844c
Checketts100 <Checketts100@users.noreply.github.com>
2018-03-08 21:00:32 -0500

    JSON HW

--- assignments/Lesson9/JSON/scripts/family.js
+++ assignments/Lesson9/JSON/scripts/family.js
@@ -7,12 +7,10 @@ family .onload = function() {
 	
 	var familyData = JSON.parse(family.responseText);
 	console.log(familyData);
-	
-	document.getElementById("wholename").innerHTML = familyData.members["0"].name;
-	document.getElementById("title").innerHTML = familyData.members["0"].familyTitle;
-	document.getElementById("age").innerHTML = familyData.members["0"].age;
-	document.getElementById("duties").innerHTML = familyData.members["0"].duties;
-	
-	
-	
+		  
+	document.getElementById("wholename").innerHTML = familyData.members[0].name;
+	document.getElementById("title").innerHTML = familyData.members[0].familyTitle;
+	document.getElementById("age").innerHTML = familyData.members[0].age;
+	document.getElementById("duties").innerHTML = familyData.members[0].duties;
+      		
 }
