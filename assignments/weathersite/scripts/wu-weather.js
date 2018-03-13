877c9be10ce97a63dab95ae54a3061c9b61267ff
Checketts <J8547@csx.com>
2018-03-13 09:09:05 -0400

    changing locations

--- assignments/weathersite/scripts/wu-weather.js
+++ assignments/weathersite/scripts/wu-weather.js
@@ -19,30 +19,11 @@ weatherObject.onload = function() {
 	document.getElementById("precip").innerHTML = weatherInfo.current_observation.precip_today_in;
 	document.getElementById("winddir").innerHTML = weatherInfo.current_observation.wind_string;
 }
-//SPRINGFIELD, MN
-var weatherObject2 = new XMLHttpRequest();
 
-weatherObject2.open('GET','http://api.wunderground.com/api/5d9809b7dda42a6c/conditions/q/MN/Springfield.json', true);
-
-weatherObject2.send();
-
-weatherObject2.onload = function() {
-	
-	var weatherInfo2 = JSON.parse(weatherObject2.responseText);
-	console.log(weatherInfo2);
-	
-	document.getElementById("place2").innerHTML = weatherInfo2.current_observation.display_location.full;
-	document.getElementById("currentTemp2").innerHTML = weatherInfo2.current_observation.temp_f;
-	document.getElementById("current2").innerHTML = weatherInfo2.current_observation.weather;
-	document.getElementById("windSpeed2").innerHTML = weatherInfo2.current_observation.wind_mph;
-	document.getElementById("weatherIcon2").src = weatherInfo2.current_observation.icon_url;
-	document.getElementById("precip2").innerHTML = weatherInfo2.current_observation.precip_today_in;
-	document.getElementById("winddir2").innerHTML = weatherInfo2.current_observation.wind_string;
-}
-//GREENVILLE, ME
+//GREENVILLE, TX
 var weatherObject1 = new XMLHttpRequest();
 
-weatherObject1.open('GET','http://api.wunderground.com/api/5d9809b7dda42a6c/conditions/q/ME/Greenville.json', true);
+weatherObject1.open('GET','http://api.wunderground.com/api/5d9809b7dda42a6c/conditions/q/TX/Greenville.json', true);
 
 weatherObject1.send();
 
@@ -58,4 +39,25 @@ weatherObject1.onload = function() {
 	document.getElementById("weatherIcon1").src = weatherInfo1.current_observation.icon_url;
 	document.getElementById("precip1").innerHTML = weatherInfo1.current_observation.precip_today_in;
 	document.getElementById("winddir1").innerHTML = weatherInfo1.current_observation.wind_string;
+}
+
+//SPRINGFIELD, OR
+var weatherObject2 = new XMLHttpRequest();
+
+weatherObject2.open('GET','http://api.wunderground.com/api/5d9809b7dda42a6c/conditions/q/OR/Springfield.json', true);
+
+weatherObject2.send();
+
+weatherObject2.onload = function() {
+	
+	var weatherInfo2 = JSON.parse(weatherObject2.responseText);
+	console.log(weatherInfo2);
+	
+	document.getElementById("place2").innerHTML = weatherInfo2.current_observation.display_location.full;
+	document.getElementById("currentTemp2").innerHTML = weatherInfo2.current_observation.temp_f;
+	document.getElementById("current2").innerHTML = weatherInfo2.current_observation.weather;
+	document.getElementById("windSpeed2").innerHTML = weatherInfo2.current_observation.wind_mph;
+	document.getElementById("weatherIcon2").src = weatherInfo2.current_observation.icon_url;
+	document.getElementById("precip2").innerHTML = weatherInfo2.current_observation.precip_today_in;
+	document.getElementById("winddir2").innerHTML = weatherInfo2.current_observation.wind_string;
 }
