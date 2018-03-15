5b40241683a2418a987addaed32de43381d81bf6
Checketts <J8547@csx.com>
2018-03-15 07:21:02 -0400

    fixed webpage

--- assignments/weathersite/scripts/wu-weather.js
+++ assignments/weathersite/scripts/wu-weather.js
@@ -21,6 +21,27 @@ weatherObject.onload = function() {
 	document.getElementById("winddir").innerHTML = weatherInfo.current_observation.wind_string;
 }
 
+//SPRINGFIELD, OR
+var weatherObject2 = new XMLHttpRequest();
+
+weatherObject2.open('GET','https://api.wunderground.com/api/5d9809b7dda42a6c/conditions/q/OR/Springfield.json', true);
+
+weatherObject2.send();
+
+weatherObject2.onload = function() {
+	
+	var weatherInfo2 = JSON.parse(weatherObject2.responseText);
+	console.log(weatherInfo2);
+	
+	document.getElementById("place2").innerHTML = weatherInfo2.current_observation.display_location.full;
+	document.getElementById("currentTemp2").innerHTML = weatherInfo2.current_observation.temp_f;
+	document.getElementById("current2").innerHTML = weatherInfo2.current_observation.weather;
+	document.getElementById("windSpeed2").innerHTML = weatherInfo2.current_observation.wind_mph;
+	document.getElementById("weatherIcon2").src = weatherInfo2.current_observation.icon_url;
+	document.getElementById("precip2").innerHTML = weatherInfo2.current_observation.precip_today_in;
+	document.getElementById("winddir2").innerHTML = weatherInfo2.current_observation.wind_string;
+}
+
 //GREENVILLE, TX
 var weatherObject1 = new XMLHttpRequest();
 
@@ -42,23 +63,4 @@ weatherObject1.onload = function() {
 	document.getElementById("winddir1").innerHTML = weatherInfo1.current_observation.wind_string;
 }
 
-//SPRINGFIELD, OR
-var weatherObject2 = new XMLHttpRequest();
 
-weatherObject2.open('GET','https://api.wunderground.com/api/5d9809b7dda42a6c/conditions/q/OR/Springfield.json', true);
-
-weatherObject2.send();
-
-weatherObject2.onload = function() {
-	
-	var weatherInfo2 = JSON.parse(weatherObject2.responseText);
-	console.log(weatherInfo2);
-	
-	document.getElementById("place2").innerHTML = weatherInfo2.current_observation.display_location.full;
-	document.getElementById("currentTemp2").innerHTML = weatherInfo2.current_observation.temp_f;
-	document.getElementById("current2").innerHTML = weatherInfo2.current_observation.weather;
-	document.getElementById("windSpeed2").innerHTML = weatherInfo2.current_observation.wind_mph;
-	document.getElementById("weatherIcon2").src = weatherInfo2.current_observation.icon_url;
-	document.getElementById("precip2").innerHTML = weatherInfo2.current_observation.precip_today_in;
-	document.getElementById("winddir2").innerHTML = weatherInfo2.current_observation.wind_string;
-}
