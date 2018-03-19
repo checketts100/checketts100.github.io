9b8b97928a111d13068ba9d70af32c31f5ee602c
Checketts <J8547@csx.com>
2018-03-19 06:02:21 -0400

    555

--- assignments/weathersite/scripts/windchill.js
+++ assignments/weathersite/scripts/windchill.js
@@ -4,37 +4,6 @@
 
 f = 35.74 + 0.6215 t - 35.75 s0.16 + 0.4275 t s0.16 
 */
-/*
-function computeNum() {
-	// Input - get input and validate if correct length and if a number
-	var textInput = get("aNum").value;
-	if (textInput.length < 3 || isNaN(textInput)) return; // Validation return nothing
-	
-	// Processing - multiply by the a random number taken to the 3rd power
-	var theNumber = parseInt(textInput); // OK - parse string to number
-	var theValue = theNumber * Math.pow(Math.random(), 3);
-	
-	// Output - output to the div and round the number value to two decimal places
-	get("output").innerHTML = "The value is <strong>" + theValue.toFixed(2) + "</strong>!";
-}
-
-function get(e) { return document.getElementById(e); } */
-
-/*	Input: user inputs tempture and wind speed
- *	Processing: convert numbers to integers
- *				run functions to show wind chill
- *				run funciton to calculate chill factor in Fahrenheit
- *	Output: out put answer of wind chill and how it factors in Fahrenheit
-*/ 
-
-
-
-/*	Input: user inputs tempture and wind speed
- *	Processing: convert numbers to integers
- *				run functions to show wind chill
- *				run funciton to calculate chill factor in Fahrenheit
- *	Output: out put answer of wind chill and how it factors in Fahrenheit
-*/ 
 
 function doInputOutput()  {
 	//captures input data from user and coverts to number
