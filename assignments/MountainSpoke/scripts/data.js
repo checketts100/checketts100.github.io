a9fdfc5f4f69308e7ebb54d0c35d21f916d7d9bb
Checketts <J8547@csx.com>
2018-04-09 09:51:58 -0400

    setting up page for JSON

--- assignments/MountainSpoke/scripts/data.js
+++ assignments/MountainSpoke/scripts/data.js
@@ -1,3 +1,14 @@
 //JavaScript Document
 //Services provided for Mountain Spoke
 
+var services = new XMLHttpRequest();
+services .open("GET", "https://checketts100.github.io/assignments/MountainSpoke/JSON/data.json" , true);
+services .send();
+services .onload = function () {
+	
+	var serviceData = JSON.parse(services.responseText);
+	console.log(serviceData);
+	
+	
+	
+}
